275bf7ffc5db39c3529ff182f9e093632ab1d8c4
triplem123 <triplem804@gmail.com>
2022-10-20 20:59:03 -0400

    finished initial wireframe for My Rooms page

--- pages/My-Rooms/my-rooms.js
+++ pages/My-Rooms/my-rooms.js
@@ -1,30 +1,37 @@
-const room_types = ["Bedroom", "Living-Room", "Den", "Kitchen"];
 const grid = document.getElementById("room-layout-board");
 
-
+const room_types = ["Master-Bedroom", "Living-Room", "Den", "Kitchen", "Downstairs-Bedroom", "Basement", "TV-Room"];
 //Generating sample data
 
 room_types.forEach((room) => {
     const c = document.createElement("div");
     c.classList.add(room);
-    c.classList.add("grid-item");
+    c.classList.add("room-type-grid-item");
     const b = document.createElement("button");
     b.type = "button";
     b.classList.add(room);
-    b.classList.add("button");
-    b.innerText = room;
+    b.classList.add("folder-button");
+
+    const i = document.createElement("span");
+    i.classList.add("glyphicon"); 
+    i.classList.add("glyphicon-folder-open");
+
+    b.appendChild(i);
+
+    b.append(document.createElement("br"));
+    b.append(room.replace("-", " "));
 
     c.appendChild(b);
     grid.appendChild(c);
 });
 
 const c = document.createElement("div");
-c.classList.add("grid-item");
+c.classList.add("room-type-grid-item");
 const b = document.createElement("button");
 b.type = "button";
 b.classList.add("create-new-room");
-b.classList.add("button");
-b.innerText = "Create a New Room";
+b.classList.add("folder-button");
+b.innerText = "Create New Room";
 
 c.appendChild(b);
 grid.appendChild(c);
