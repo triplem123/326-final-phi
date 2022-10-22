755281e21365e750d0602b4b399c308a090b3dae
triplem123 <triplem804@gmail.com>
2022-10-22 18:18:11 -0400

    adding assets and making some changes

--- pages/Home-NotLoggedIn/signup-popup.js
+++ pages/Home-NotLoggedIn/signup-popup.js
@@ -1,5 +1,6 @@
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+    window.scrollTo(0, document.body.scrollHeight)
 }
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
