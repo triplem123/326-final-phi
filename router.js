5635b02053cb0836fe4ec6ab602e1b35d2a1f166
triplem123 <90787133+triplem123@users.noreply.github.com>
2022-11-10 15:00:52 -0500

    commit

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.post(__dirname + '/pages/html/home-notloggedin.html');
+    res.sendFile(__dirname + '/pages/html/home-notloggedin.html');
 });
 
 router.get('/home-notloggedin.html', (req, res) => {
