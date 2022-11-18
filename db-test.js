3cbbff85e26969422e181490a97ef9a0dce743bb
AryunGupta <aryungupta@gmail.com>
2022-11-18 17:13:42 -0500

    test

---
+++ db-test.js
@@ -0,0 +1,40 @@
+// schema.js begins here- editing that file for testing
+
+import { ObjectId } from "mongodb";
+import mongoose, { Promise, connect } from "mongoose";
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+Promise = global.Promise;
+// connect("mongodb://localhost:3000/team-phi");
+mongoose.connect("mongodb://localhost:3000");
+// mongoose.connect("mongodb://localhost/team-phi");
+
+// creating the schema
+const userSchema = new Schema({
+    email: String,
+    emailhash: ObjectId,
+    password: String,
+    num_room_layouts: Number,
+    room_designs: ObjectId
+});
+// model from the schema
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
+
+// building CRUD endpoint
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// saving data to database
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save().then(item => {
+        res.send("item saved to database");
+    }).catch(err => {
+        res.status(400).send("unable to save to database");
+    });
+});
