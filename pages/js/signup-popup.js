63bf1eb8ac48a11ffe599dce0c8acf79df4fd10f
triplem123 <triplem804@gmail.com>
2022-12-11 17:21:19 -0500

    should be working now :) pt. 2

--- pages/js/signup-popup.js
+++ pages/js/signup-popup.js
@@ -40,7 +40,7 @@ async function validUser() {
     if (username.length <= 1 || password.length <= 1) {
         return false;
     } else {
-        await fetch('http://localhost:3000/login', {
+        await fetch('https://roomio-room-builder.herokuapp.com/login', {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
